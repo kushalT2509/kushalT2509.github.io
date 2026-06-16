ca61cd1cc46949fbe7b4286363aeeca417f7c687
kushalT2509 <kushal.takoordyal@umail.utm.ac.mu>
2026-06-16 09:18:19 +0400

    c

--- script.js
+++ script.js
@@ -6,11 +6,22 @@
 }); 
 */
 $(document).ready(function () {
+
+    // Toggle main hamburger menu
     $('.hamburger').on('click', function () {
         $('.menu').toggleClass('showing');
     });
-});
 
+    // Toggle the Categories submenu
+    $('.submenu-title').on('click', function () {
+        $('.submenu-items').slideToggle(200);
+
+        // Change + to - when opened
+        let plus = $(this).find('.plus');
+        plus.text(plus.text() === '+' ? '-' : '+');
+    });
+
+});
 
 
 /*
